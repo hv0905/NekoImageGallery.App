4d7273deee6b8b9ddba69dddbf1c5aeb05357f46
EdgeNeko <edgeneko@aiursoft.com>
2023-12-09 00:45:52 +0800

    Add strictNullChecks to ESLint and tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
