8b7994d695a2d9dd9fd644e58ac618e573ce78b3
EdgeNeko <edgeneko@aiursoft.com>
2023-12-15 15:52:33 +0800

    Add Paging feature

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
 import { SearchResult } from '../Models/SearchResult';
 import { ImageGallery } from './ImageGallery';
 import { QueryArea } from './QueryArea';
@@ -6,37 +6,68 @@ import { SearchQuery } from '../Services/SearchQuery';
 import { AuthenticationDialog } from './AuthenticationDialog';
 import { WelcomeApi } from '../Services/WelcomeApi';
 import { HomeApiResponse } from '../Models/HomeApiResponse';
+import { Box, Button, CircularProgress } from '@mui/material';
 
 export const ApiInfo = createContext<HomeApiResponse | null>(null);
 
 export function Home() {
+  const activeQuery = useRef<SearchQuery | null>(null);
   const [result, setResult] = useState<SearchResult[] | null>(null);
   const [apiInfo, setApiInfo] = useState<HomeApiResponse | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [noMore, setNoMore] = useState(false);
+
+  function queryNext(reset = false) {
+    if (!activeQuery.current) return;
+    setLoading(true)
+    void activeQuery.current.querySearch(30,reset ? 0 : result?.length ?? 0).then(t => {
+      if (reset) {
+        console.log("Reset!");
+        setResult(t.result);
+      } else {
+        setResult([...(result ?? []), ...t.result]);
+      }
+      if (t.result.length < 30) {
+        setNoMore(true);
+      }
+    }).finally(() => {
+      setLoading(false);
+    });
+  }
 
   function search(query: SearchQuery) {
-    void query.querySearch(30).then(t => {
-      setResult(t.result);
-    });
+    activeQuery.current = query;
+    queryNext(true);
   }
 
   useEffect(() => {
     void WelcomeApi().then(resp => {
       setApiInfo(resp);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <ApiInfo.Provider value={apiInfo}>
       <QueryArea onSubmit={search}></QueryArea>
 
       {result ? (
-        <ImageGallery
-          searchResult={result}
-          onSimilarSearch={search}
-        ></ImageGallery>
+        <>
+          <ImageGallery
+            searchResult={result}
+            onSimilarSearch={search}
+          ></ImageGallery>
+          {loading ? (
+            <Box display='flex' justifyContent='center' sx={{width: '100%'}}>
+            <CircularProgress sx={{ my: '20px' }} />
+            </Box>
+          ) : ( !noMore &&
+            <Button fullWidth sx={{ my: '20px' }} onClick={() => queryNext()}>
+              Load More
+            </Button>
+          )}
+        </>
       ) : null}
-
-      <AuthenticationDialog/>
+      <AuthenticationDialog />
     </ApiInfo.Provider>
   );
 }
--- src/Services/SearchQuery.ts
+++ src/Services/SearchQuery.ts
@@ -4,17 +4,22 @@ import { SearchBasis } from '../Models/SearchBasis';
 import { getClient } from './Base';
 
 export abstract class SearchQuery {
-  abstract querySearch(count: number): Promise<SearchApiResponse>;
+  abstract querySearch(count: number, skip: number): Promise<SearchApiResponse>;
 }
 
 export class TextSearchQuery extends SearchQuery {
-  constructor(public query: string, public searchBasis: SearchBasis = SearchBasis.vision) {
+  constructor(
+    public query: string,
+    public searchBasis: SearchBasis = SearchBasis.vision
+  ) {
     super();
   }
 
-  async querySearch(count = 20): Promise<SearchApiResponse> {
+  async querySearch(count = 20, skip = 0): Promise<SearchApiResponse> {
     const response = await getClient().get<SearchApiResponse>(
-      `/search/text/${encodeURIComponent(this.query)}?count=${count}&basis=${this.searchBasis}`
+      `/search/text/${encodeURIComponent(
+        this.query
+      )}?count=${count}&skip=${skip}&basis=${this.searchBasis}`
     );
     return response.data;
   }
@@ -25,7 +30,7 @@ export class ImageSearchQuery extends SearchQuery {
     super();
   }
 
-  async querySearch(count = 20): Promise<SearchApiResponse> {
+  async querySearch(count = 20, skip = 0): Promise<SearchApiResponse> {
     const formData = new FormData();
     formData.append('image', this.image);
     const response = await getClient().post<SearchApiResponse>(
@@ -37,6 +42,7 @@ export class ImageSearchQuery extends SearchQuery {
         },
         params: {
           count: count,
+          skip: skip,
         },
       }
     );
@@ -45,13 +51,18 @@ export class ImageSearchQuery extends SearchQuery {
 }
 
 export class SimilarSearchQuery extends SearchQuery {
-  constructor(public id: string, public searchBasis: SearchBasis = SearchBasis.vision) {
+  constructor(
+    public id: string,
+    public searchBasis: SearchBasis = SearchBasis.vision
+  ) {
     super();
   }
 
-  async querySearch(count = 20): Promise<SearchApiResponse> {
+  async querySearch(count = 20, skip = 0): Promise<SearchApiResponse> {
     const response = await getClient().get<SearchApiResponse>(
-      `/search/similar/${encodeURIComponent(this.id)}?count=${count}&basis=${this.searchBasis}`
+      `/search/similar/${encodeURIComponent(
+        this.id
+      )}?count=${count}&skip=${skip}&basis=${this.searchBasis}`
     );
     return response.data;
   }
@@ -71,17 +82,21 @@ export class RandomSearchQuery extends SearchQuery {
 }
 
 export class AdvancedSearchQuery extends SearchQuery {
-  constructor(public searchModel: AdvancedSearchModel, public searchBasis: SearchBasis = SearchBasis.vision) {
+  constructor(
+    public searchModel: AdvancedSearchModel,
+    public searchBasis: SearchBasis = SearchBasis.vision
+  ) {
     super();
   }
 
-  async querySearch(count = 20): Promise<SearchApiResponse> {
+  async querySearch(count = 20, skip = 0): Promise<SearchApiResponse> {
     const response = await getClient().post<SearchApiResponse>(
       `/search/advanced`,
       this.searchModel,
       {
         params: {
           count: count,
+          skip: skip,
           basis: this.searchBasis,
         },
       }
