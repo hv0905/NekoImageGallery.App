12c3509a2fad9ffc4126d29f2a090932fbf22a6a
EdgeNeko <edgeneko@aiursoft.com>
2023-12-19 19:16:45 +0800

    Add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Neko Image Gallery",
+        short_name: "NekoGallery",
+        description: "An AI-powered natural language & reverse Image Search Engine.",
+        theme_color: "#2196f3",
+        icons: [
+          {
+            src: 'pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   envDir: './env',
 })
