01f6ce8a11d8d8053da9205cd46f105ba935210f
EdgeNeko <edgeneko@aiursoft.com>
2024-07-12 19:31:23 +0800

    Better override-on-the-fly solution

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,28 @@
+ARG API_URL=/api
+ARG APP_DISPLAY_NAME=NekoImageGallery
+
 FROM hub.aiursoft.cn/node:21-alpine as yarn-env
 WORKDIR /app
 COPY ./package.json ./yarn.lock ./
 RUN yarn install --frozen-lockfile
 COPY . .
+
+ARG API_URL
+ARG APP_DISPLAY_NAME
+
+ENV VITE_API_URL=${API_URL} \
+    VITE_APP_DISPLAY_NAME=${APP_DISPLAY_NAME}
+
 RUN yarn run build
 
 FROM hub.aiursoft.cn/aiursoft/static
 COPY --from=yarn-env /app/dist /data
+
+ARG API_URL
+ARG APP_DISPLAY_NAME
+
+ENV VITE_API_URL=${API_URL} \
+    VITE_APP_DISPLAY_NAME=${APP_DISPLAY_NAME}
 
 COPY entrypoint.sh /app/entrypoint.sh
 RUN chmod +x /app/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 # print all environment variables
 
-if [ -n "$VITE_API_URL" ]; then
-  echo "Overriding VITE_API_URL with $VITE_API_URL"
-  sed -i "s|ApiUrl:\"http://localhost:8000\"|ApiUrl:\"$VITE_API_URL\"|g" /data/assets/index-*.js
+if [ -n "$OVERRIDE_API_URL" ]; then
+  echo "Overriding VITE_API_URL($VITE_API_URL) with $OVERRIDE_API_URL"
+  sed -i "s|ApiUrl:\"$VITE_API_URL\"|ApiUrl:\"$OVERRIDE_API_URL\"|g" /data/assets/index-*.js
 fi
 
-if [ -n "$VITE_APP_DISPLAY_NAME" ]; then
-  echo "Overriding VITE_APP_DISPLAY_NAME with $VITE_APP_DISPLAY_NAME"
-  sed -i "s|AppDisplayName:\"Neko Image Gallery\"|AppDisplayName:\"$VITE_APP_DISPLAY_NAME\"|g" /data/assets/index-*.js
+if [ -n "$OVERRIDE_APP_DISPLAY_NAME" ]; then
+  echo "Overriding VITE_APP_DISPLAY_NAME($VITE_APP_DISPLAY_NAME) with $OVERRIDE_APP_DISPLAY_NAME"
+  sed -i "s|AppDisplayName:\"$VITE_APP_DISPLAY_NAME\"|AppDisplayName:\"$OVERRIDE_APP_DISPLAY_NAME\"|g" /data/assets/index-*.js
 fi
 
 /app/static --port 5000 --path /data --not-found-page /
