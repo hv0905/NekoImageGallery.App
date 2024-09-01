d90f96316388466e2b46d4213cba231c95b385c5
EdgeNeko <edgeneko@aiursoft.com>
2024-09-02 02:22:20 +0800

    feat: Allow drop & paste directories

--- src/Hooks/useFileDropper.ts
+++ src/Hooks/useFileDropper.ts
@@ -1,34 +1,60 @@
 import {DragEvent, ClipboardEvent} from 'react';
 
+function globFiles(item: FileSystemEntry, base = ''): Promise<File[]> {
+  if (item.isFile) {
+    return new Promise(resolve => {
+      (<FileSystemFileEntry>item).file(file => {
+        resolve([file]);
+      });
+    });
+  } else if (item.isDirectory) {
+    return new Promise(resolve => {
+      const reader = (<FileSystemDirectoryEntry>item).createReader();
+      reader.readEntries(async entries => {
+        const files = await Promise.all(entries.map(t => globFiles(t, base + item.name + '/')));
+        resolve(files.flat());
+      });
+    });
+  } else {
+    return Promise.resolve([]);
+  }
+}
+
 export function useFileDropper(
   fileAccept: string[] = ['*/*'],
   onSuccess?: (files: File[]) => void,
   onIncorrectType?: () => void
 ) {
+
+  async function processList(items: DataTransferItemList){
+    const files = (
+      await Promise.all(
+        Array.from(items ?? [])
+          .map(t => t.webkitGetAsEntry())
+          .filter(t => !!t)
+          .map(t => globFiles(t))
+      )
+    )
+      .flat()
+      .filter(t => fileAccept.includes(t.type));
+
+    if (files.length > 0) {
+      onSuccess?.(files);
+    } else {
+      onIncorrectType?.();
+    }
+  }
+
   return {
-    onDrop(e: DragEvent<HTMLElement>) {
+    async onDrop(e: DragEvent<HTMLElement>) {
       e.preventDefault();
-      const files = Array.from(e.dataTransfer.files ?? []).filter(f => fileAccept.includes(f.type));
-      if (files.length > 0) {
-        onSuccess?.(files);
-      } else {
-        onIncorrectType?.();
-      }
+      await processList(e.dataTransfer.items);
     },
     onDragOver(e: DragEvent<HTMLElement>) {
       e.preventDefault();
     },
-    onPaste(e: ClipboardEvent<HTMLDivElement>) {
-      const files = Array.from(e.clipboardData.items ?? [])
-        .filter(t => t.kind === 'file')
-        .map(t => t.getAsFile())
-        .filter((t): t is File => !!t && fileAccept.includes(t.type));
-
-      if (files.length > 0) {
-        onSuccess?.(files);
-      } else {
-        onIncorrectType?.();
-      }
+    async onPaste(e: ClipboardEvent<HTMLDivElement>) {
+      await processList(e.clipboardData.items);
     },
   };
 }
