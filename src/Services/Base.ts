c35a3802609bddf2764aec1344238b68617aa2d8
EdgeNeko <edgeneko@aiursoft.com>
2025-07-12 00:29:43 +0800

    Add withCredentials to support oAuth scenerios

--- src/Services/Base.ts
+++ src/Services/Base.ts
@@ -14,6 +14,7 @@ export function getClient() {
         'X-Access-Token': token ?? undefined,
         'X-Admin-Token': adminToken ?? undefined,
       },
+      withCredentials: true
     });
   }
 
